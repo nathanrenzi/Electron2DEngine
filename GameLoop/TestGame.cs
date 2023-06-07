7c4f3ddd826023a5e08444a30f54c371d018e35b
Electronicall <electronicallyt@gmail.com>
2023-06-07 00:11:41 -0400

    Basic Image Loading

--- GameLoop/TestGame.cs
+++ GameLoop/TestGame.cs
@@ -1,9 +1,11 @@
 ﻿using static OpenGLTest.OpenGL.GL;
 using GLFW;
+using StbImageSharp;
 using OpenGLTest.Rendering.Display;
 using OpenGLTest.Rendering.Shaders;
 using OpenGLTest.Rendering.Cameras;
 using System.Numerics;
+using System.Reflection;
 
 namespace OpenGLTest.GameLoop
 {
@@ -29,8 +31,10 @@ namespace OpenGLTest.GameLoop
         {
             string vertexShader = @"#version 330 core
                                     layout (location = 0) in vec2 aPosition;
-                                    layout (location = 1) in vec3 aColor;
+                                    layout (location = 1) in vec2 aTexCoord;
+                                    layout (location = 2) in vec3 aColor;
                                     out vec4 vertexColor;
+                                    out vec2 texCoord;
     
                                     uniform mat4 projection;
                                     uniform mat4 model;
@@ -39,15 +43,20 @@ namespace OpenGLTest.GameLoop
                                     {
                                         vertexColor = vec4(aColor.rgb, 1.0);
                                         gl_Position = projection * model * vec4(aPosition.xy, 0, 1.0);
+                                        texCoord = aTexCoord;
                                     }";
 
             string fragmentShader = @"#version 330 core
                                     out vec4 FragColor;
+
                                     in vec4 vertexColor;
+                                    in vec2 texCoord;
+
+                                    uniform sampler2D mainTexture;
 
                                     void main() 
                                     {
-                                        FragColor = vertexColor;
+                                        FragColor = texture(mainTexture, texCoord);
                                     }";
 
             shader = new Shader(vertexShader, fragmentShader);
@@ -62,13 +71,14 @@ namespace OpenGLTest.GameLoop
 
             float[] vertices =
             {
-                -0.5f, 0.5f, 1f, 0f, 0f, // top left
-                0.5f, 0.5f, 0f, 1f, 0f,// top right
-                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+                // Position    UV        Color
+                -0.5f, 0.5f,   0f, 0f,   1f, 0f, 0f,    // top left
+                0.5f, 0.5f,    1f, 0f,   0f, 1f, 0f,    // top right
+                -0.5f, -0.5f,  0f, 1f,   0f, 0f, 1f,    // bottom left
 
-                0.5f, 0.5f, 0f, 1f, 0f,// top right
-                0.5f, -0.5f, 0f, 1f, 1f, // bottom right
-                -0.5f, -0.5f, 0f, 0f, 1f, // bottom left
+                0.5f, 0.5f,    1f, 0f,   0f, 1f, 0f,    // top right
+                0.5f, -0.5f,   1f, 1f,   0f, 1f, 1f,    // bottom right
+                -0.5f, -0.5f,  0f, 1f,   0f, 0f, 1f,    // bottom left
             };
 
             fixed (float* v = &vertices[0])
@@ -76,18 +86,54 @@ namespace OpenGLTest.GameLoop
                 glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, v, GL_STATIC_DRAW);
             }
 
-            glVertexAttribPointer(0, 2, GL_FLOAT, false, 5 * sizeof(float), (void*)0);
+            // Vertices
+            glVertexAttribPointer(0, 2, GL_FLOAT, false, 7 * sizeof(float), (void*)0);
             glEnableVertexAttribArray(0);
 
-            glVertexAttribPointer(1, 3, GL_FLOAT, false, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+            // UV
+            glVertexAttribPointer(1, 2, GL_FLOAT, false, 7 * sizeof(float), (void*)(2 * sizeof(float)));
             glEnableVertexAttribArray(1);
+
+            // Colors
+            glVertexAttribPointer(2, 3, GL_FLOAT, false, 7 * sizeof(float), (void*)(4 * sizeof(float)));
+            glEnableVertexAttribArray(2);
+
 
             glBindBuffer(GL_ARRAY_BUFFER, 0);
             glBindVertexArray(0);
 
+            // Binding texture to uint
+            uint texture;
+            glGenTextures(1, &texture);
+            glBindTexture(GL_TEXTURE_2D, texture);
+
+            // Setting texture wrapping / filtering options on bound texture object
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            // Loading and generating texture
+            string imagesPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Images\\");
+            string path = Path.Combine(imagesPath, "test.png");
+            byte[] data = File.ReadAllBytes(path);
+            ImageResult image = ImageResult.FromMemory(data, ColorComponents.RedGreenBlueAlpha);
+            fixed (byte* d = image.Data)
+            {
+                if (d != null)
+                {
+                    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.Width, image.Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, d);
+                    glGenerateMipmap(GL_TEXTURE_2D);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to load texture.");
+                }
+            }
+
             cam = new Camera2D(DisplayManager.windowSize / 2, 2.5f);
         }
-
+         
         protected override void Update()
         {
 
