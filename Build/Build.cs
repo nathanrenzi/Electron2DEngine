a0902b791d0b87c97034c4694de8eb9865a1faf0
nathanrenzi <54221741+nathanrenzi@users.noreply.github.com>
2024-10-25 17:54:57 -0400

    Cleaned build

--- Build/Build.cs
+++ Build/Build.cs
@@ -7,8 +7,6 @@ using System.Numerics;
 
 public class Build : Game
 {
-    private Tilemap tilemap;
-
     public Build(int _initialWindowWidth, int _initialWindowHeight) : base(_initialWindowWidth, _initialWindowHeight,
         $"Electron2D Build - {Program.BuildDate}", _vsync: false, _antialiasing: false, _physicsPositionIterations: 4, _physicsVelocityIterations: 8,
         _errorCheckingEnabled: true, _showElectronSplashscreen: true)
@@ -25,36 +23,12 @@ public class Build : Game
     protected override void Load()
     {
         SetBackgroundColor(Color.FromArgb(255, 80, 80, 80));
-
-        int[] tiles = new int[100];
-        Array.Fill(tiles, -1);
-        StartCamera.Zoom = 4;
-        tilemap = Tilemap.CreateSharedMaterial(Material.Create(GlobalShaders.DefaultTexture,
-            ResourceManager.Instance.LoadTexture("Build/Resources/autotile.png")),
-            new TileData[] {new TileData("Test", _ruleset: new DefaultTilemapRuleset()).SetAsCollider(new Electron2D.Core.PhysicsBox2D.RigidbodyStaticDef()),
-                new TileData("Test2", _ruleset: new DefaultTilemapRuleset()) }, tiles,
-            16, 10, 10);
     }
 
     // This is ran every frame
     protected override void Update()
     {
-        if(Input.GetMouseButtonDown(GLFW.MouseButton.Left))
-        {
-            Vector2 pos = Input.GetMouseWorldPosition();
-            if(pos.X <= 160 && pos.X >= 0 && pos.Y <= 160 && pos.Y >= 0)
-            {
-                tilemap.SetTileID((int)(pos.X / 16), (int)(pos.Y / 16), 0);
-            }
-        }
-        if (Input.GetMouseButtonDown(GLFW.MouseButton.Right))
-        {
-            Vector2 pos = Input.GetMouseWorldPosition();
-            if (pos.X <= 160 && pos.X >= 0 && pos.Y <= 160 && pos.Y >= 0)
-            {
-                tilemap.SetTileID((int)(pos.X / 16), (int)(pos.Y / 16), 1);
-            }
-        }
+
     }
 
     // This is ran every frame right before rendering
