80c6e639e90b28306830d966e11da1ad0baa5744
nathanrenzi <54221741+nathanrenzi@users.noreply.github.com>
2025-10-17 00:52:09 -0400

    Refactor GameScene to SceneGroup

--- Source/Misc/SceneGroup.cs
+++ Source/Misc/SceneGroup.cs
@@ -3,37 +3,40 @@
 namespace Electron2D
 {
     /// <summary>
-    /// A scene of <see cref="IGameClass"/> and <see cref="UIComponent"/> objects that can be enabled, disabled, or disposed.
+    /// A group of <see cref="IGameClass"/> and/or <see cref="UIComponent"/> objects that can be enabled, disabled, or disposed.
     /// </summary>
-    public abstract class GameScene : IGameClass
+    public abstract class SceneGroup : IGameClass
     {
         private List<IGameClass> _classes = new List<IGameClass>();
         private List<UIComponent> _uiComponents = new List<UIComponent>();
         private bool[] _uiVisibilityState = null;
         protected bool _enabled = false;
 
-        public GameScene()
+        public SceneGroup()
         {
             Engine.Game.RegisterGameClass(this);
-            CreateGameClasses();
+            OnLoad();
         }
 
-        ~GameScene()
+        ~SceneGroup()
         {
             Dispose();
         }
 
         /// <summary>
-        /// Should create any game classes that should be added to the scene. Make sure to call <see cref="Register(IGameClass)"/>
-        /// on every game class created.
+        /// Should create any objects that should be added to the group. Make sure to call <see cref="Register"/>
+        /// on every <see cref="IGameClass"/> or <see cref="UIComponent"/> created.
         /// </summary>
-        protected abstract void CreateGameClasses();
+        protected abstract void OnLoad();
+        protected abstract void OnEnable();
+        protected abstract void OnDisable();
         protected abstract void OnUpdate();
         protected abstract void OnFixedUpdate();
         protected abstract void OnDispose();
 
         public void Register(IGameClass gameClass)
         {
+            if (_classes == null || _uiComponents == null) return;
             if (gameClass == this) return;
             if (_classes.Contains(gameClass)) return;
             Engine.Game.UnregisterGameClass(gameClass);
@@ -42,25 +45,29 @@ namespace Electron2D
 
         public void Register(UIComponent uiComponent)
         {
+            if (_classes == null || _uiComponents == null) return;
             if (_uiComponents.Contains(uiComponent)) return;
             _uiComponents.Add(uiComponent);
         }
 
         public void Unregister(IGameClass gameClass)
         {
+            if (_classes == null || _uiComponents == null) return;
             _classes.Remove(gameClass);
         }
 
         public void Unregister(UIComponent uiComponent)
         {
+            if (_classes == null || _uiComponents == null) return;
             _uiComponents.Remove(uiComponent);
         }
 
         /// <summary>
-        /// Enables the scene, and all registered objects.
+        /// Enables the group, and all registered objects.
         /// </summary>
         public void Enable()
         {
+            if (_classes == null || _uiComponents == null) return;
             _enabled = true;
             if(_uiVisibilityState != null)
             {
@@ -69,13 +76,15 @@ namespace Electron2D
                     _uiComponents[i].Visible = _uiVisibilityState[i];
                 }
             }
+            OnEnable();
         }
 
         /// <summary>
-        /// Disables the scene, and all registered objects.
+        /// Disables the group, and all registered objects.
         /// </summary>
         public void Disable()
         {
+            if (_classes == null || _uiComponents == null) return;
             _enabled = false;
             _uiVisibilityState = new bool[_uiComponents.Count];
             for (int i = 0; i < _uiComponents.Count; i++)
@@ -83,10 +92,12 @@ namespace Electron2D
                 _uiVisibilityState[i] = _uiComponents[i].Visible;
                 _uiComponents[i].Visible = false;
             }
+            OnDisable();
         }
 
         public void Update()
         {
+            if (_classes == null || _uiComponents == null) return;
             if (!_enabled) return;
             for(int i = 0; i < _classes.Count; i++)
             {
@@ -97,6 +108,7 @@ namespace Electron2D
 
         public void FixedUpdate()
         {
+            if (_classes == null || _uiComponents == null) return;
             if (!_enabled) return;
             for (int i = 0; i < _classes.Count; i++)
             {
@@ -106,10 +118,11 @@ namespace Electron2D
         }
 
         /// <summary>
-        /// Disposes the scene, and all registered objects.
+        /// Disposes the group, and all registered objects.
         /// </summary>
         public void Dispose()
         {
+            if (_classes == null || _uiComponents == null) return;
             GC.SuppressFinalize(this);
             Engine.Game.UnregisterGameClass(this);
             for (int i = 0; i < _classes.Count; i++)
