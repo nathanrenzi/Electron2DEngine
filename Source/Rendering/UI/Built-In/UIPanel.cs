c243ebf44838df99fd235df4c7eab2365aea74c1
Nathan Renzi <54221741+nathanrenzi@users.noreply.github.com>
2026-02-01 21:39:39 -0500

    Add built-in UIPanel and UISlicedPanel

---
+++ Source/Rendering/UI/Built-In/UIPanel.cs
@@ -0,0 +1,73 @@
+﻿using Electron2D.Rendering;
+using System.Drawing;
+
+namespace Electron2D.UI
+{
+    public class UIPanel : UIElement
+    {
+        private float[] _vertices = new float[16];
+        private static readonly uint[] _indices =
+        {
+            3, 1, 0,
+            3, 2, 1
+        };
+
+        public UIPanel(ITexture texture, int sizeX = 0, int sizeY = 0, int uiRenderLayer = 0, bool useScreenPosition = true, bool ignorePostProcessing = true)
+            : base(sizeX, sizeY, uiRenderLayer, useScreenPosition, ignorePostProcessing, true)
+        {
+            Renderer.Material.MainTexture = texture;
+            UpdateMesh();
+            Renderer.SetVertexArrays(_vertices, _indices);
+        }
+
+        public UIPanel(Color color, int sizeX = 0, int sizeY = 0, int uiRenderLayer = 0, bool useScreenPosition = true, bool ignorePostProcessing = true)
+            : base(sizeX, sizeY, uiRenderLayer, useScreenPosition, ignorePostProcessing, true)
+        {
+            SetColor(color);
+            UpdateMesh();
+            Renderer.SetVertexArrays(_vertices, _indices);
+        }
+
+        public UIPanel(Material material, int sizeX = 0, int sizeY = 0, int uiRenderLayer = 0, bool useScreenPosition = true, bool ignorePostProcessing = true)
+            : base(sizeX, sizeY, uiRenderLayer, useScreenPosition, ignorePostProcessing, true)
+        {
+            Renderer.SetMaterial(material);
+            UpdateMesh();
+            Renderer.SetVertexArrays(_vertices, _indices);
+        }
+
+        public override void UpdateMesh()
+        {
+            float left = -Pivot.X * Size.X;
+            float right = left + Size.X;
+            float top = -Pivot.Y * Size.Y;
+            float bottom = top + Size.Y;
+
+            // Top Left
+            _vertices[0] = left;
+            _vertices[1] = top;
+            _vertices[2] = 0f;
+            _vertices[3] = 0f;
+
+            // Top Right
+            _vertices[4] = right;
+            _vertices[5] = top;
+            _vertices[6] = 1f;
+            _vertices[7] = 0f;
+
+            // Bottom Right
+            _vertices[8] = right;
+            _vertices[9] = bottom;
+            _vertices[10] = 1f;
+            _vertices[11] = 1f;
+
+            // Bottom Left
+            _vertices[12] = left;
+            _vertices[13] = bottom;
+            _vertices[14] = 0f;
+            _vertices[15] = 1f;
+
+            Renderer.IsVertexDirty = true;
+        }
+    }
+}
