f8fe33bf46bcac1308d81dc84517ad9a53f284b6
Nathan Renzi <54221741+nathanrenzi@users.noreply.github.com>
2026-02-01 21:39:19 -0500

    Add built-in UIContainer Used for child positioning without rendering

---
+++ Source/Rendering/UI/Built-In/UIContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace Electron2D.UI
+{
+    public class UIContainer : UIElement
+    {
+        public UIContainer(int sizeX = 0, int sizeY = 0, int uiRenderLayer = 0, bool useScreenPosition = true)
+            : base(sizeX, sizeY, uiRenderLayer, useScreenPosition, true, false)
+        { }
+
+        public override void UpdateMesh() { }
+    }
+}
