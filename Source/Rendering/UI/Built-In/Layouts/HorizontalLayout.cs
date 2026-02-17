3212021b98f66d79e8bcfb7afdb52470fe7dfc82
Nathan Renzi <54221741+nathanrenzi@users.noreply.github.com>
2026-02-16 21:59:59 -0500

    Add horizontal and vertical list layouts

---
+++ Source/Rendering/UI/Built-In/Layouts/HorizontalLayout.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Electron2D.UI
+{
+    public sealed class HorizontalLayout : LinearLayout
+    {
+        protected override float GetMain(Vector2 v) => v.X;
+        protected override float GetCross(Vector2 v) => v.Y;
+        protected override Vector2 MakeVector(float main, float cross) => new Vector2(main, cross);
+    }
+}
---
+++ Source/Rendering/UI/Built-In/Layouts/VerticalLayout.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Electron2D.UI
+{
+    public sealed class VerticalLayout : LinearLayout
+    {
+        protected override float GetMain(Vector2 v) => v.Y;
+        protected override float GetCross(Vector2 v) => v.X;
+        protected override Vector2 MakeVector(float main, float cross) => new Vector2(cross, main);
+    }
+}
