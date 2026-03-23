c99d6570ca3207117dbb9dd16f725791da2ff89c
Nathan Renzi <54221741+nathanrenzi@users.noreply.github.com>
2026-03-23 14:52:58 -0400

    Remove TextAlignmentMode from UIText

--- Source/Rendering/UI/Built-In/Styles/UITextStyle.cs
+++ Source/Rendering/UI/Built-In/Styles/UITextStyle.cs
@@ -10,20 +10,18 @@ namespace Electron2D.UI
         public FontArgs FontArguments { get; }
         public TextAlignment HorizontalAlignment { get; }
         public TextAlignment VerticalAlignment { get; }
-        public TextAlignmentMode AlignmentMode { get; }
         public TextOverflowMode OverflowMode { get; }
         public float LineHeightMultiplier { get; }
         public Shader? CustomShader { get; }
 
         public UITextStyle(Color color, FontArgs fontArguments, TextAlignment horizontalAlignment = TextAlignment.Left,
-            TextAlignment verticalAlignment = TextAlignment.Top, TextAlignmentMode alignmentMode = TextAlignmentMode.Baseline,
-            TextOverflowMode overflowMode = TextOverflowMode.Word, float lineHeightMultiplier = 1.35f, Shader? customShader = null)
+            TextAlignment verticalAlignment = TextAlignment.Top, TextOverflowMode overflowMode = TextOverflowMode.Word,
+            float lineHeightMultiplier = 1.35f, Shader? customShader = null)
         {
             Color = color;
             FontArguments = fontArguments;
             HorizontalAlignment = horizontalAlignment;
             VerticalAlignment = verticalAlignment;
-            AlignmentMode = alignmentMode;
             OverflowMode = overflowMode;
             LineHeightMultiplier = lineHeightMultiplier;
             CustomShader = customShader;
